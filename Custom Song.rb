1fbd9a7c2b4ca06cf565a98be7aefae5691ccbd1
CreatingGreatThings <124717920+CreatingGreatThings@users.noreply.github.com>
2023-02-28 11:42:10 -0800

    Update Custom Song.rb

--- Custom Song.rb
+++ Custom Song.rb
@@ -1,5 +1,58 @@
 # Custom Song
-live_loop :line1 do
+with_fx :flanger do
+  use_bpm 148
+  use_synth :tech_saws
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+end
+
+use_synth :tech_saws
+play:e5
+sleep 1
+play:d5
+sleep 1
+play:e5
+sleep 1
+play:d5
+sleep 1
+play:e5
+sleep 1
+play:d5
+sleep 1
+play:e5
+sleep 1
+play:d5
+sleep 1
+play:e5
+sleep 1
+play:d5
+sleep 1
+play:e5
+sleep 1
+play:d5
+
+amp_fade = (ring 2, 1.5, 1, 0.5, 0)
+
+live_loop :treble do
   use_bpm 148
   sleep 1
   play:e5, sustain: 2
@@ -22,17 +75,34 @@ live_loop :line1 do
   sleep 1
   play:d5, sustain: 2
   sleep 1
+  play:gs4, sustain: 4, amp: amp_fade
+  play:d4, sustain: 4, amp: amp_fade
   stop
 end
 
-live_loop :line2 do
+live_loop :base do
   play:a3
   play:c3
   sleep 1
   play:e3
   sleep 1
-  play:a3
-  play:c3
+  play:a3, sustain: 2
+  play:c3, sustain: 2
+  sleep 1
+  play:a3, sustain: 2
+  play:c3, sustain: 2
+  sleep 1
+  play:e3, sustain: 2
+  sleep 1
+  play:es3
+  play:gs3
+  sleep 1
+  play:b3
+  sleep 1
+  play:e3, sustain: 2
+  play:g3, sustain: 2
+  sleep 1
+  play:e3
   sleep 1
   stop
 end
