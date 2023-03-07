f3d75b990fcccd2216a46de0efa39ad0764185e3
CreatingGreatThings <124717920+CreatingGreatThings@users.noreply.github.com>
2023-03-07 10:33:53 -0800

    Update ChallengeF.rb

--- ChallengeF.rb
+++ ChallengeF.rb
@@ -1,19 +1,19 @@
 num = 21
 
 use_synth :piano
-88.times do
+87.times do
   play(num)
   sleep 0.3
   num = num + 1
 end
 
-88.times do
+87.times do
   play(num)
   sleep 0.15
   num = num - 1
 end
 
-89.times do
+88.times do
   play(num)
   sleep 0.1
   num = num + 1
