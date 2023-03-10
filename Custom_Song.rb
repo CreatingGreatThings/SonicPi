273f3cb287a0533df44df254e48b5ec1391decbc
CreatingGreatThings <124717920+CreatingGreatThings@users.noreply.github.com>
2023-03-10 11:43:27 -0800

    Create Custom_Song.rb

---
+++ Custom_Song.rb
@@ -0,0 +1,113 @@
+lyrics = "C:/Users/marcelo_aeschbacher/Desktop/.wav.wav"
+
+with_fx :flanger do
+  use_bpm 148
+  use_synth :tech_saws
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+end
+
+use_synth :tech_saws
+play:e5
+sleep 1
+play:d5
+sleep 1
+play:e5
+sleep 1
+play:d5
+sleep 1
+play:e5
+sleep 1
+play:d5
+sleep 1
+play:e5
+sleep 1
+play:d5
+sleep 1
+play:e5
+sleep 1
+play:d5
+sleep 1
+play:e5
+sleep 1
+play:d5
+
+amp_fade = (ring 2, 1.5, 1, 0.5, 0)
+
+1.times do
+  lyrics
+end
+
+live_loop :treble do
+  use_bpm 148
+  sleep 1
+  play:e5, sustain: 2
+  sleep 1
+  play:e5
+  sleep 1
+  play:d5, sustain: 2
+  sleep 1
+  play:d5
+  sleep 1
+  play:e5
+  sleep 1
+  play:c5
+  sleep 1
+  play:d5, sustain: 2
+  sleep 1
+  play:e5
+  sleep 1
+  play:e5
+  sleep 1
+  play:d5, sustain: 2
+  sleep 1
+  play:gs4, sustain: 4, amp: amp_fade
+  play:d4, sustain: 4, amp: amp_fade
+  stop
+end
+
+live_loop :base do
+  play:a3
+  play:c3
+  sleep 1
+  play:e3
+  sleep 1
+  play:a3, sustain: 2
+  play:c3, sustain: 2
+  sleep 1
+  play:a3, sustain: 2
+  play:c3, sustain: 2
+  sleep 1
+  play:e3, sustain: 2
+  sleep 1
+  play:es3
+  play:gs3
+  sleep 1
+  play:b3
+  sleep 1
+  play:e3, sustain: 2
+  play:g3, sustain: 2
+  sleep 1
+  play:e3
+  sleep 1
+  stop
+end
