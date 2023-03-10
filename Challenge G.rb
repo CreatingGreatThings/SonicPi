5d9ec18e1410ba54f4f66e3fef476a26154742d1
CreatingGreatThings <124717920+CreatingGreatThings@users.noreply.github.com>
2023-03-10 15:37:13 -0800

    Update Challenge G.rb

--- Challenge G.rb
+++ Challenge G.rb
@@ -1,14 +1,17 @@
-use_bpm 160
-use_synth :saw
+use_bpm 120
+use_synth :fm
+use_synth_defaults sustain: 0.25
 
-notes = [:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2, :c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2]
+notes = [:e3,:e3,:g3,:e3,:r,:d3,:c3,:b2]
+slp = [1.5, 0.5, 0.75, 0.25, 0.5, 0.5, 2, 2]
 index = 0
 
-live_loop :main_theme do
-  with_fx :distortion do
+live_loop :white_stripes do
+  8.times do
     play notes[index]
-    sleep 0.5
+    sleep slp[index]
     index = index + 1
     puts(index)
   end
+  index = 0
 end
