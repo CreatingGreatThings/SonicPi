386a6498f59f815b0fcc78e069077b9c32f11583
CreatingGreatThings <124717920+CreatingGreatThings@users.noreply.github.com>
2023-04-11 15:32:44 -0700

    Update 2nd song.rb

--- 2nd song.rb
+++ 2nd song.rb
@@ -1,7 +1,7 @@
 use_bpm 156
 use_synth :sine
 
-define :notes do |n1, n2, n3, n4, n5, n6, n7, n8|
+define :notes do |n1, n2, n3, n4, n5, n6, n7, n8, n9, n10|
   play n1
   sleep 1
   play n2
@@ -11,13 +11,23 @@ define :notes do |n1, n2, n3, n4, n5, n6, n7, n8|
   play n4
   sleep 1
   play n5
-  sleep 1.5
+  sleep 1
+  play n6
+  sleep 1
+  play n7
+  sleep 1
+  play n8
+  sleep 1
+  play n9
+  sleep 1
+  play n10
+  sleep 1
 end
 
 
 live_loop :an do
   1.times do
-    notes :
+    notes :c5, :f5, : 
       notes :
       notes :
       stop
