f3f313543aad28fa9e2a671b530af9b358587dbe
CreatingGreatThings <124717920+CreatingGreatThings@users.noreply.github.com>
2023-04-14 11:42:33 -0700

    Create Array42ndSong.rb

---
+++ Array42ndSong.rb
@@ -0,0 +1,13 @@
+use_synth :piano
+use_bpm 156
+
+notes = [:f4, :a5, :a5, :a5, :a5, :c5, :c5, :eb5, :eb5, :f5]
+index = 0
+
+live_loop :measure2 do
+  1.times do
+    play notes[index]
+    sleep slp[index]
+    index = index + 1
+    puts(index)
+  end
