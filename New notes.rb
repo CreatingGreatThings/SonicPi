4667615fec80423bbb82293067ea1601a20d471f
CreatingGreatThings <124717920+CreatingGreatThings@users.noreply.github.com>
2023-04-14 10:44:28 -0700

    Update New notes.rb

--- New notes.rb
+++ New notes.rb
@@ -1,5 +1,5 @@
 use_synth :piano
-use_bpm 156
+use_bpm 160
 
 live_loop :notes do
   sleep 0.5
@@ -7,21 +7,21 @@ live_loop :notes do
   sleep 0.5
   play :f5
   sleep 1
-  play :f5 #sustain: 2
+  play :f5, sustain: 2
   sleep 1
-  play :f5 #sustain: 2
+  play :f5, sustain: 2
   sleep 1
-  play :f5
-  sleep 1
-  play :f5
-  sleep 1
-  play :eb5
-  sleep 1
-  play :db5
+  play :c5
+  sleep 0.6
+  play :c5
+  sleep 0.6
+  play :c5
   sleep 0.6
   play :eb5
   sleep 0.6
-  play :f5
+  play :eb5
+  sleep 0.6
+  play :f5, sustain: 2
   sleep 1
   stop
 end
