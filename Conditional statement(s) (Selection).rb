a5aa6b1850836370e655a41733c470fd1a45a194
CreatingGreatThings <124717920+CreatingGreatThings@users.noreply.github.com>
2023-04-17 21:51:50 -0700

    Create Conditional statement(s) (Selection).rb

---
+++ Conditional statement(s) (Selection).rb
@@ -0,0 +1,35 @@
+a = 5
+
+5.times do
+  if a == 5
+    sample :drum_bass_soft
+  else
+    sample :drum_bass_hard
+  end
+  
+  sleep 1
+end
+
+a = 2
+
+5.times do
+  if a == 5
+    sample :drum_bass_soft
+  else
+    sample :drum_bass_hard
+  end
+  
+  sleep 1
+end
+
+a = 5
+
+5.times do
+  if a == 5
+    sample :drum_bass_soft
+  else
+    sample :drum_bass_hard
+  end
+  
+  sleep 1
+end
