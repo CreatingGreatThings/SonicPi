731b2a299b0ccc94a5b2a1404993cf731727b709
CreatingGreatThings <124717920+CreatingGreatThings@users.noreply.github.com>
2023-04-18 12:57:58 -0700

    Update Conditional_S.rb

--- Conditional_S.rb
+++ Conditional_S.rb
@@ -2,14 +2,17 @@ a = 0
 
 live_loop :drum do
   15.times do
-    if a == 5 or 10
+    if a < 5
       sample :drum_bass_soft
-    else
+    elsif a > 5
       sample :drum_bass_hard
+    else a >= 10
+      sample :drum_bass_soft
     end
     
     sleep 1
     a = a + 1
     puts(a)
   end
+  stop
 end
